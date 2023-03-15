541604386ccf4108f43f6f06c7251e2ceee95dec
doannucphys <doan.phys@gmail.com>
2023-03-15 11:19:16 +0700

    update old funciton

--- logic_generate_payment/buildPaymentsequence.js
+++ logic_generate_payment/buildPaymentsequence.js
@@ -11,7 +11,10 @@ const buildTransactions = (
     rate = 1,
     reimFreq = "every_two_weeks"
 ) => {
-    let arr = [];
+
+    let fees = 0
+    console.log(`sequence || currBalance || amount  || interest  || capital     || fees     || balance ||`)
+    console.log('----------------------------------------------------------------------------------------')
     let singleRate = rate / frequency[reimFreq];
     console.log(singleRate, initInsNb, initInsAmt);
     let vf =
@@ -19,22 +22,25 @@ const buildTransactions = (
         singleRate;
     let balance = vf;
     for (let i = 1; i <= initInsNb; i++) {
+        let currBalance = balance
         let currInt = balance * singleRate;
         let capital = initInsAmt - currInt;
         balance -= capital;
-        arr.push(
-            JSON.stringify({
-                sequence: i,
-                installDate: new Date(),
-                amount: parseFloat(initInsAmt).toFixed(2),
-                interest: parseFloat(currInt).toFixed(2),
-                capital: parseFloat(capital).toFixed(2),
-                balance: parseFloat(balance).toFixed(2),
-                status: "pending",
-            })
-        );
+        console.log(`${i < 10? "0" : ''}${i}        || ${parseFloat(currBalance).toFixed(2)}      || ${parseFloat(initInsAmt).toFixed(2)}   ||${parseFloat(currInt).toFixed(2)}       || ${parseFloat(capital).toFixed(2)}       || ${fees}    || ${parseFloat(balance).toFixed(2)} || ${"pending"}`);
     }
-    return arr;
+        // arr.push(
+        //     JSON.stringify({
+        //         sequence: i,
+        //         installDate: new Date(),
+        //         amount: parseFloat(initInsAmt).toFixed(2),
+        //         interest: parseFloat(currInt).toFixed(2),
+        //         capital: parseFloat(capital).toFixed(2),
+        //         balance: parseFloat(balance).toFixed(2),
+        //         status: "pending",
+        //     })
+        // );
+    // }
+    // return arr;
 };
 
 const updateTransaction = (sequences = [], arr = []) => {
