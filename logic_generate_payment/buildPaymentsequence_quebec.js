668dff97c21745414d06933590ea24fc3353d567
doannucphys <doan.phys@gmail.com>
2023-03-15 11:12:18 +0700

    ud for quebec

--- logic_generate_payment/buildPaymentsequence_quebec.js
+++ logic_generate_payment/buildPaymentsequence_quebec.js
@@ -7,54 +7,50 @@ let frequency = {
 };
 
 const buildTransactions = (
+    loans = 0,
+    fees = 20,
     initInsAmt = 0,
     initInsNb = 1,
     rate = 1,
     reimFreq = "every_two_weeks"
 ) => {
-    let arr = [];
+    console.log(`sequence || currBalance || amount  || interest  || capital     || fees     || balance ||`)
+    console.log('----------------------------------------------------------------------------------------')
     let singleRate = rate / frequency[reimFreq];
-    console.log(singleRate, initInsNb, initInsAmt);
-    let vf =
-        (initInsAmt * (1 - 1 / Math.pow(1 + singleRate, initInsNb))) /
-        singleRate;
-    let balance = vf;
+    let balance = loans;
+    // console.log(singleRate, initInsNb, initInsAmt);
+    // let vf =
+    //     (initInsAmt * (1 - 1 / Math.pow(1 + singleRate, initInsNb))) /
+    //     singleRate;
+    // let balance = vf;
     for (let i = 1; i <= initInsNb; i++) {
+        let currBalance = balance;
         let currInt = balance * singleRate;
-        let capital = initInsAmt - currInt;
+        let capital = initInsAmt - currInt - fees;
         balance -= capital;
-        arr.push(
-            JSON.stringify({
-                sequence: i,
-                installDate: new Date(),
-                amount: parseFloat(initInsAmt).toFixed(2),
-                interest: parseFloat(currInt).toFixed(2),
-                capital: parseFloat(capital).toFixed(2),
-                balance: parseFloat(balance).toFixed(2),
-                status: "pending",
-            })
-        );
-    }
-    return arr;
-};
-
-const updateTransaction = (sequences = [], arr = []) => {
-    if (sequences && sequences.length && arr && arr.length) {
-        // for (let itr = 0; itr < arr.length; itr++) {
-        for (let ise = 0; ise < sequence.length; ise++) {
-            // if (sequences[ise] == arr[itr].sequence) {
-            // }
-        }
-        // }
+        console.log(`${i < 10? "0" : ''}${i}        || ${parseFloat(currBalance).toFixed(2)}      || ${parseFloat(initInsAmt).toFixed(2)}   ||${parseFloat(currInt).toFixed(2)}       || ${parseFloat(capital).toFixed(2)}       || ${fees}    || ${parseFloat(balance).toFixed(2)} || ${"pending"}`);
     }
 };
 
+//  ----------------- for other region ------------------
+// let amt = 36.57;
+// let totalOwing = 438.8;
+// let initInsNb = 13;
+// let initInsAmt = parseFloat(totalOwing / initInsNb).toFixed(2);
+// let rate = 0.29;
+// let reimFreq = "every_week";
+
+//  ----------------- for quebec region ------------------
 let amt = 36.57;
-let totalOwing = 438.8;
-let initInsNb = 12;
-let initInsAmt = parseFloat(totalOwing / initInsNb).toFixed(2);
+let totalOwing = 500;
+let loans = 500 // for quebec need to add this
+let fees = 20
+let initInsNb = 13;
 let rate = 0.29;
+let initInsAmt = 59.98
+// totalOwing/((1- Math.pow((1+rate/52),-initInsNb))/rate)
 let reimFreq = "every_week";
 
-const trans = buildTransactions(initInsAmt, initInsNb, rate, reimFreq);
-console.log(trans);
+
+buildTransactions(loans,fees, initInsAmt, initInsNb, rate, reimFreq);
+
